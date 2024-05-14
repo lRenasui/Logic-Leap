d6b2c8db892d4add3404f22427ed637d1ba4395b
pt1C3 <rafaelsilva1c3@gmail.com>
2024-05-14 11:16:50 +0100

    breadcrumbs conditions

--- frontend/src/components/breadcrumbs.js
+++ frontend/src/components/breadcrumbs.js
@@ -2,19 +2,24 @@
 import './breadcrumbs.css';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons"; 
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
-function Breadcrumbs({ page1, page2, page3, link1,link2 }) {
+function Breadcrumbs({ page1, page2, page3, link1, link2 }) {
     return (
         <nav aria-label="breadcrumb" class="breadcrumb-wrapper px-10vw">
             <ol className="breadcrumb">
                 <li className="breadcrumb-item"><FontAwesomeIcon icon={faHouse} /></li>
                 <li className="breadcrumb-item"><Link to={link1}>{page1}</Link></li>
-                <li className="breadcrumb-item"><Link to={link2}>{page2}</Link></li>
-                <li className="breadcrumb-item active">{page3}</li>
+                {link2 ?
+                    <li className="breadcrumb-item"><Link to={link2}>{page2}</Link></li>
+                    : <li className="breadcrumb-item active">{page2}</li>
+                }
+                {page3 ? <li className="breadcrumb-item active">{page3}</li>: undefined}
 
-            </ol>
-        </nav>
+            
+
+        </ol>
+        </nav >
     );
 }
 
