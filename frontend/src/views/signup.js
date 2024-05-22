064b221f7c94c635b77927c4f8da9b5f7da481f1
lRenasui <107072037+lRenasui@users.noreply.github.com>
2024-05-22 13:54:54 +0100

    mudanças

--- frontend/src/views/signup.js
+++ frontend/src/views/signup.js
@@ -1,88 +1,119 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SignUp() {
-    return(
-           <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-body">
-                            <h3 className="card-title text-left"><strong>Sign up</strong></h3>
-                            <p className="text-left">Already have an account? <a href="#">Login</a></p>
-                            <form>
-                                <div className="form-row">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="firstName">First name</label>
-                                        <input type="text" className="form-control mr-3" id="firstName" placeholder="António" />
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="lastName">Last name</label>
-                                        <input type="text" className="form-control" id="lastName" placeholder="Mendes" />
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email" className="form-control" id="email" placeholder="antoniomendes@empresa.pt" />
-                                    <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <input type="password" className="form-control" id="password" placeholder="********" />
-                                </div>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="confirmPassword">Confirm Password</label>
-                                    <input type="password" className="form-control" id="confirmPassword" placeholder="********" />
-                                </div>
-                                <div className="form-row">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="country">Country</label>
-                                        <select id="country" className="form-control">
-                                            <option value="United States">Estados Unidos</option>
-                                            <option value="United Kingdom">Inglaterra</option>
-                                            <option value="Canada">Canadá</option>
-                                            <option value="Australia">Australia</option>
-                                            <option value="Germany">Alemanha</option>
-                                            <option value="France">França</option>
-                                            <option value="Italy">Itália</option>
-                                            <option value="Spain">Espanha</option>
-                                            <option value="Brazil">Brasil</option>
-                                            <option value="China">China</option>
-                                            <option value="India">India</option>
-                                            <option value="Japan">Japão</option>
-                                            <option value="Russia">Russia</option>
-                                            <option selected>Portugal</option>
-                                            <option>...</option>
-                                        </select>
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="phoneNumber">Phone number</label>
-                                        <input type="text" className="form-control" id="phoneNumber" placeholder="+351 999999999" />
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" name="role" id="business" value="business" defaultChecked />
-                                        <label className="form-check-label" htmlFor="business">Business</label>
-                                    </div>
-                                    <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" name="role" id="manager" value="manager" />
-                                        <label className="form-check-label" htmlFor="manager">Manager</label>
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="businessName">Business’ name</label>
-                                    <input type="text" className="form-control" id="businessName" placeholder="Inteligência Lda." />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="website">Business’ website</label>
-                                    <input type="text" className="form-control" id="website" placeholder="inteligencialda.pt" />
-                                </div>
-                                <button type="submit" className="btn btn-primary w-100 mt-1">Sign Up</button>
-                            </form>
-                        </div>
-                    </div>
+  return (
+<div className="container mt-5">
+  <div className="row justify-content-center">
+    <div className="col-md-8">
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title text-left"><strong>Sign up</strong></h3>
+          <p className="text-left">Already have an account? <a href="#">Login</a></p>
+          <form>
+            <div className="row g-3">
+              <div className="col-sm-6">
+                <label htmlFor="firstName" className="form-label">
+                  First Name
+                </label>
+                <input type="text" className="form-control" id="firstName" placeholder="António" />
+              </div>
+              <div className="col-sm-6">
+                <label htmlFor="lastName" className="form-label">
+                  Last Name
+                </label>
+                <input type="text" className="form-control" id="lastName" placeholder="Mendes" />
+              </div>
+              <div className="col-sm-12">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="antoniomendes@empresa.pt"
+                />
+              </div>
+              <div className="col-sm-12">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                />
+              </div>
+              <div className="col-sm-12">
+                <label htmlFor="confirmPassword" className="form-label">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="confirmPassword"
+                />
+              </div>
+              <div className="col-sm-6">
+                <label htmlFor="country" className="form-label">
+                  Country
+                </label>
+                <select className="form-select form-control" id="country">
+                  <option selected>Portugal</option>
+                </select>
+              </div>
+              <div className="col-sm-6">
+                <label htmlFor="phoneNumber" className="form-label">
+                  Phone number
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="phoneNumber"
+                  placeholder="+351 999999999"
+                />
+              </div>
+              <div className="form-group">
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="radio" name="role" id="business" value="business" defaultChecked />
+                  <label className="form-check-label" htmlFor="business">Business</label>
                 </div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="radio" name="role" id="manager" value="manager" />
+                  <label className="form-check-label" htmlFor="manager">Manager</label>
+                </div>
+              </div>
+              <div className="col-sm-12">
+                <label htmlFor="businessName" className="form-label">
+                  Business' name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="businessName"
+                  placeholder="Inteligência Lda."
+                />
+              </div>
+              <div className="col-sm-12">
+                <label htmlFor="businessWebsite" className="form-label">
+                  Business' website
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="businessWebsite"
+                  placeholder="inteligencialda.pt"
+                />
+              </div>
             </div>
+            <button type="submit" className="btn-primary w-100 mt-1">
+              Sign up
+            </button>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  </div>
+</div>
+  );
 }
-
